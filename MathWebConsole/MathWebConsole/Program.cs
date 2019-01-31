806e5ae743333aef4bae6e2de382ab6fcf17d29d
Ajeeth Kumar <ajeeth.kumar@SGZ.com>
2019-01-31 17:13:15 +0530

    Assignment

---
+++ MathWebConsole/MathWebConsole/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathWebConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //myservicereference.MyService obj = new myservicereference.MyService();
+            localhost.WebService1 obj = new localhost.WebService1();
+            Console.WriteLine(" Calling Web Methods");
+            Console.WriteLine("---------------------");
+            Console.WriteLine("\n Calling show Method");
+            Console.WriteLine("  " + obj.show());
+            Console.WriteLine("\n\n" + " Calling showdate Method ");
+            Console.WriteLine("  " + obj.showdate());
+            Console.WriteLine("\n Calling add Method ");
+            Console.Write("  Enter First Number:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("  Enter Second Number:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n  Addition is:" + obj.add(a, b).ToString());
+
+            //Console.WriteLine("\n Calling Subtract Method ");
+            //Console.Write("  Enter First Number:");
+            //a = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("  Enter Second Number:");
+            //b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n  Subtratcion is:" + obj.Subtract(a, b).ToString());
+            Console.Write("  \n\nEnter List of no's you want to add with (seperate no's with space)space ");
+            string listNum = Console.ReadLine();
+            Console.WriteLine("\n Addition of n no's  is:" + obj.addMoreThan2Numbers(listNum));
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Mathservice/Mathservice1/MathService.asmx.cs
@@ -0,0 +1,56 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace Mathservice1
+{
+   
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+        [WebMethod(Description = "show simple message")]
+        public string show()
+        {
+            return "Web sevice Application";
+        }
+        [WebMethod(Description = "show datetime")]
+        public string showdate()
+        {
+            return DateTime.Now.ToLongDateString();
+        }
+        [WebMethod(Description = "addition of two int")]
+        public double add(double a, double b)
+        {
+
+            return a + b;
+        }
+
+        [WebMethod(Description = "addition of n numbers")]
+        public double addMoreThan2Numbers(string numbers)
+        {
+
+            string[] listNumbers = numbers.Split(' ');
+
+            double result = 0;
+            for (int i = 0; i < listNumbers.Length; i++)
+            {
+                result += Convert.ToDouble(listNumbers[i]);
+            }
+            return result;
+        }
+
+
+        [WebMethod(Description = "Substraction of two int")]
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+    }
+}
